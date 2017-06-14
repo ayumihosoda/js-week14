fd66337fd7205226cc0d8ef81f08a16d8091ff9f
Ayumi <ayumihosoda@gmail.com>
2017-06-13 19:36:46 -0700

    First commit

---
+++ js/main.js
@@ -0,0 +1,31 @@
+// 6. In JS: Create a function to add the map, with one variable for map properties. Update the location to your favorite place on Earth.
+
+function initialize() {
+	var mapProp = {
+		center: new google.maps.LatLng(40.5546558,14.21052839999993),
+		zoom: 5,
+//14. In JS: Disable the default controls on the map
+		disableDefaultUI: true,
+		mapTypeId: google.maps.MapTypeId.ROADMAP
+
+	}
+// 8. In JS: Add the map to the screen using the google.maps.Map method.
+	var map=new google.maps.Map(document.getElementById('googleMap'),mapProp)
+
+// 9. In JS: Create a marker and set the position to mapProp.center. Add the marker to the map using the Google setMap method.
+	var marker = new google.maps.Marker({
+			position: mapProp.center
+		})
+
+	marker.setMap(map)
+
+// 12. In JS: Using the google.maps.event.addListener method, add an event so when you click on the marker it zooms in on the map marker.
+	google.maps.event.addListener(marker, 'click', function() {
+		map.setZoom(9)
+		map.setCenter(marker.getPosition())
+	})
+	}
+
+//7 In JS: Using the google.maps.event.addDomListener method, select the window and when it loads run the function to add the map.
+	google.maps.event.addDomListener(window, 'load', initialize)
+
